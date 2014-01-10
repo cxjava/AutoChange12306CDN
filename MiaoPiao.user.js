f1a17e7aec855d04e7c29dde0a66a12ccbaf833e
Charles <cxjava@gmail.com>
2014-01-10 22:44:41 +0800

    fix bug about the station name
    change the refresh time from 1000 to 2000

--- MiaoPiao.user.js
+++ MiaoPiao.user.js
@@ -1,33 +1,33 @@
 ﻿/*
-*  12306 Auto Query => A javascript snippet to help you book tickets online.
-*  12306 Booking Assistant
-*  Copyright (C) 2011 Hidden
-* 
-*  12306 Auto Query => A javascript snippet to help you book tickets online.
-*  Copyright (C) 2011 Jingqin Lynn
-* 
-*  12306 Auto Login => A javascript snippet to help you auto login 12306.com.
-*  Copyright (C) 2011 Kevintop
-* 
-*  Includes jQuery
-*  Copyright 2011, John Resig
-*  Dual licensed under the MIT or GPL Version 2 licenses.
-*  http://jquery.org/license
-* 
-*  This program is free software: you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation, either version 3 of the License, or
-*  (at your option) any later version.
-* 
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-* 
-*  You should have received a copy of the GNU General Public License
-*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-* 
-*/
+ *  12306 Auto Query => A javascript snippet to help you book tickets online.
+ *  12306 Booking Assistant
+ *  Copyright (C) 2011 Hidden
+ *
+ *  12306 Auto Query => A javascript snippet to help you book tickets online.
+ *  Copyright (C) 2011 Jingqin Lynn
+ *
+ *  12306 Auto Login => A javascript snippet to help you auto login 12306.com.
+ *  Copyright (C) 2011 Kevintop
+ *
+ *  Includes jQuery
+ *  Copyright 2011, John Resig
+ *  Dual licensed under the MIT or GPL Version 2 licenses.
+ *  http://jquery.org/license
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 // ==UserScript==
 // @name         12306 果果秒票助手 V3
@@ -41,194 +41,200 @@
 
 function withjQuery(callback, safe) {
 
-    if (typeof (jQuery) == "undefined") {
+	if (typeof(jQuery) == "undefined") {
 
-        var script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "https://ajax.aspnetcdn.com/ajax/jquery/jquery-1.7.1.min.js";
+		var script = document.createElement("script");
+		script.type = "text/javascript";
+		script.src = "https://ajax.aspnetcdn.com/ajax/jquery/jquery-1.7.1.min.js";
 
-        if (safe) {
-            var cb = document.createElement("script");
-            cb.type = "text/javascript";
-            cb.textContent = "jQuery.noConflict();(" + callback.toString() + ")(jQuery, window);";
+		if (safe) {
+			var cb = document.createElement("script");
+			cb.type = "text/javascript";
+			cb.textContent = "jQuery.noConflict();(" + callback.toString() + ")(jQuery, window);";
 
-            script.addEventListener('load', function () {
+			script.addEventListener('load', function() {
 
-                document.head.appendChild(cb);
+				document.head.appendChild(cb);
 
-            });
-        }
-        else {
-            var dollar = undefined;
-            if (typeof ($) != "undefined") dollar = $;
-            script.addEventListener('load', function () {
-                jQuery.noConflict();
-                $ = dollar;
-                callback(jQuery, window);
-            });
-        }
-        document.head.appendChild(script);
-    } else {
+			});
+		} else {
+			var dollar = undefined;
+			if (typeof($) != "undefined") dollar = $;
+			script.addEventListener('load', function() {
+				jQuery.noConflict();
+				$ = dollar;
+				callback(jQuery, window);
+			});
+		}
+		document.head.appendChild(script);
+	} else {
 
-        setTimeout(function () {
-            //Firefox supports
-            callback(jQuery, typeof unsafeWindow === "undefined" ? window : unsafeWindow);
-        }, 30);
-    }
+		setTimeout(function() {
+			//Firefox supports
+			callback(jQuery, typeof unsafeWindow === "undefined" ? window : unsafeWindow);
+		}, 30);
+	}
 }
 
-withjQuery(function ($, window) {
+withjQuery(function($, window) {
 
-    $(document).click(function () {
-        if (window.webkitNotifications && window.webkitNotifications.checkPermission() != 0) {
-            window.webkitNotifications.requestPermission();
-        }
-    });
+	$(document).click(function() {
+		if (window.webkitNotifications && window.webkitNotifications.checkPermission() != 0) {
+			window.webkitNotifications.requestPermission();
+		}
+	});
 
-    function notify(str, timeout, skipAlert) {
-        if (window.webkitNotifications && window.webkitNotifications.checkPermission() == 0) {
-            var notification = webkitNotifications.createNotification(
-				"http://www.12306.cn/mormhweb/images/favicon.ico",  // icon url - can be relative
-				'订票',  // notification title
+	function notify(str, timeout, skipAlert) {
+		if (window.webkitNotifications && window.webkitNotifications.checkPermission() == 0) {
+			var notification = webkitNotifications.createNotification(
+				"http://www.12306.cn/mormhweb/images/favicon.ico", // icon url - can be relative
+				'订票', // notification title
 				str
 			);
-            notification.show();
-            if (timeout) {
-                setTimeout(function () {
-                    notification.cancel();
-                }, timeout);
-            }
-            return true;
-        } else {
-            if (!skipAlert) {
-                //alert(str);
-            }
-            return false;
-        }
-    }
-    function route(match, fn) {
-        if (window.location.href.indexOf(match) != -1) {
-            fn();
-        };
-    }
+			notification.show();
+			if (timeout) {
+				setTimeout(function() {
+					notification.cancel();
+				}, timeout);
+			}
+			return true;
+		} else {
+			if (!skipAlert) {
+				//alert(str);
+			}
+			return false;
+		}
+	}
 
-    var clickevent = document.createEvent('MouseEvents');
-    clickevent.initEvent('click', false, true);
+	function route(match, fn) {
+		if (window.location.href.indexOf(match) != -1) {
+			fn();
+		};
+	}
 
-    // --- 设置cookie
-    var setCookie = function (sName, sValue, expireHours) {
-        var cookieString = sName + "=" + escape(sValue);
-        //;判断是否设置过期时间
-        if (expireHours > 0) {
-            var date = new Date();
-            date.setTime(date.getTime() + expireHours * 3600 * 1000);
-            cookieString = cookieString + "; expires=" + date.toGMTString();
+	var clickevent = document.createEvent('MouseEvents');
+	clickevent.initEvent('click', false, true);
 
-
-        }
-        document.cookie = cookieString + "; path=/";
-
-    }
-
-    //--- 获取cookie
-    var getCookie = function (sName) {
-        var aCookie = document.cookie.split("; ");
-        for (var j = 0; j < aCookie.length; j++) {
-            var aCrumb = aCookie[j].split("=");
-            if (escape(sName) == aCrumb[0]) {
-                if (aCrumb[1])
-                    return unescape(aCrumb[1]);
-            }
-        }
-        return "";
-    }
-
-    function refreshStation() {
-        var stationobj = {};
-        var stationnames = window.station_names.split("@");
-        for (var i in stationnames) {
-            var stationname = stationnames[i];
-            if (stationname == "")
-                continue;
-
-            try {
-                var stationfields = stationname.split("|");
-                stationobj[stationfields[1]] = stationfields[2];
-            } catch (e) {
-
-            }
-        }
-
-        var stationGroups = {
-            "北京": ["北京北", "北京东", "北京南", "北京", "北京西"],
-            "长沙": ["长沙", "长沙南"],
-            "衡阳": ["衡阳", "衡阳东"],
-            "重庆": ["重庆", "重庆北"],
-            "武汉": ["武汉", "武昌", "汉口"],
-            "桂林": ["桂林", "桂林北"],
-            "广州": ["广州", "广州东", "广州南"]
-        };
-
-        var queryTimes = 1;
-
-        var resetStation = function (station) {
-
-            for (var i in stationGroups) {
-                var stations = stationGroups[i];
-                if ($(station + "Text").val().indexOf(i) == 0) {
-                    var stationText = stations[Math.round(Math.random() * (stations.length - 1))];
-                    $(station + "Text").val(stationText);
-                    $(station).val(stationobj[stationText]);
-                    break;
-                }
-            }
-        }
-
-        setInterval(function () {
-            if ($("#query_ticket").text() == "停止查询") {
-                resetStation("#fromStation");
-                resetStation("#toStation");
-                queryTimes++;
-            }
-        }, 1000);
-    }
+	// --- 设置cookie
+	var setCookie = function(sName, sValue, expireHours) {
+		var cookieString = sName + "=" + escape(sValue);
+		//;判断是否设置过期时间
+		if (expireHours > 0) {
+			var date = new Date();
+			date.setTime(date.getTime() + expireHours * 3600 * 1000);
+			cookieString = cookieString + "; expires=" + date.toGMTString();
 
 
-    function query() {
-        refreshStation();
+		}
+		document.cookie = cookieString + "; path=/";
 
-        setTimeout(function () { window.autoSearchTime = 1000; }, 2000);
+	}
 
-        setInterval(function () {
-            if ($("#autosubmitcheckticketinfo").css("display") == "none" && $("#query_ticket").text() == "停止查询") {
-                document.getElementById("query_ticket").dispatchEvent(clickevent);
-               
-                setTimeout(function () { document.getElementById("query_ticket").dispatchEvent(clickevent); }, 1000);
-            }
-        }, 5000);
+	//--- 获取cookie
+	var getCookie = function(sName) {
+		var aCookie = document.cookie.split("; ");
+		for (var j = 0; j < aCookie.length; j++) {
+			var aCrumb = aCookie[j].split("=");
+			if (escape(sName) == aCrumb[0]) {
+				if (aCrumb[1])
+					return unescape(aCrumb[1]);
+			}
+		}
+		return "";
+	}
 
-        //        setInterval(function () {
-        //            if ($("#autosubmitcheckticketinfo").length > 0 && $("#autosubmitcheckticketinfo").css("display") != "none") {
+		function refreshStation() {
+			var stationobj = {};
+			var stationnames = window.station_names.split("@");
+			for (var i in stationnames) {
+				var stationname = stationnames[i];
+				if (stationname == "")
+					continue;
 
-        //                if ($("#music").length == 0) {
-        //                    $("#autosubmitcheckticketinfo").append("<iframe id='music' src='http://bz.5sing.com/1471807.html' height='0' width='0'>");
-        //                }
+				try {
+					var stationfields = stationname.split("|");
+					stationobj[stationfields[1]] = stationfields[2];
+				} catch (e) {
 
-        //                if ($("#randCode2").length > 0) {
-        //                    if (!$("#randCode2").val()) {
-        //                        $("#randCode2")[0].focus();
-        //                    }
-        //                }
+				}
+			}
 
-        //                if ($("#i-ok2").length > 0 && $("#i-ok2").css("display") == "block") {
-        //                    document.getElementById("qr_submit").dispatchEvent(clickevent);
-        //                }
-        //            }
+			var stationGroups = {
+				"北京": ["北京北", "北京东", "北京南", "北京", "北京西"],
+				"长沙": ["长沙", "长沙南"],
+				"衡阳": ["衡阳", "衡阳东"],
+				"重庆": ["重庆", "重庆北"],
+				"武汉": ["武汉", "武昌", "汉口"],
+				"武昌": ["武汉", "武昌", "汉口"],
+				"汉口": ["武汉", "武昌", "汉口"],
+				"桂林": ["桂林", "桂林北"],
+				"广州": ["广州", "广州东", "广州南"]
+			};
 
-        //        }, 500);
-    }
+			var queryTimes = 1;
 
-    route("init", query);
+			var resetStation = function(station) {
+
+				for (var i in stationGroups) {
+					var stations = stationGroups[i];
+					if ($(station + "Text").val().indexOf(i) == 0) {
+						var stationText = stations[Math.round(Math.random() * (stations.length - 1))];
+						$(station + "Text").val(stationText);
+						$(station).val(stationobj[stationText]);
+						break;
+					}
+				}
+			}
+
+			setInterval(function() {
+				if ($("#query_ticket").text() == "停止查询") {
+					resetStation("#fromStation");
+					resetStation("#toStation");
+					queryTimes++;
+				}
+			}, 2000);
+		}
+
+
+		function query() {
+			refreshStation();
+
+			setTimeout(function() {
+				window.autoSearchTime = 2000;
+			}, 2000);
+
+			setInterval(function() {
+				if ($("#autosubmitcheckticketinfo").css("display") == "none" && $("#query_ticket").text() == "停止查询") {
+					document.getElementById("query_ticket").dispatchEvent(clickevent);
+
+					setTimeout(function() {
+						document.getElementById("query_ticket").dispatchEvent(clickevent);
+					}, 1000);
+				}
+			}, 5000);
+
+			//        setInterval(function () {
+			//            if ($("#autosubmitcheckticketinfo").length > 0 && $("#autosubmitcheckticketinfo").css("display") != "none") {
+
+			//                if ($("#music").length == 0) {
+			//                    $("#autosubmitcheckticketinfo").append("<iframe id='music' src='http://bz.5sing.com/1471807.html' height='0' width='0'>");
+			//                }
+
+			//                if ($("#randCode2").length > 0) {
+			//                    if (!$("#randCode2").val()) {
+			//                        $("#randCode2")[0].focus();
+			//                    }
+			//                }
+
+			//                if ($("#i-ok2").length > 0 && $("#i-ok2").css("display") == "block") {
+			//                    document.getElementById("qr_submit").dispatchEvent(clickevent);
+			//                }
+			//            }
+
+			//        }, 500);
+		}
+
+	route("init", query);
 
 
 }, true);
